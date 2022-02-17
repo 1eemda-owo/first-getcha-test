847510f6182c4e04370eccd3efc7ceb9134beb0b
Dahye Lee <philoe2455@naver.com>
2022-02-17 16:20:28 +0900

    api2

--- src/App.js
+++ src/App.js
@@ -1,40 +1,44 @@
-import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
+import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-class App extends React.Component{
-  state={
-    movies:[]
+class App extends React.Component {
+  state = {
+    apiUrl:
+      "https://yts.mx/api/v2/list_movies.json?sort_by=like_count&limit=15&order_by=desc",
+    inLoading: true,
+    movies: [],
   };
 
   getMovies = async () => {
-    const movies = await axios.get("https://yts.mx/api/v2/list_movies.xml?sort=seeds&limit=15");
-    this.setState({movies});
+    const {data:{data:{movies}}} = await axios.get(this.state.apiUrl);
+    this.setState({ movies, isLoading: false });
   }
 
   componentDidMount() {
     this.getMovies();
   }
 
-  render(){
-    const { movies } = this.state;
-    return (
-      <section className="box">
-        <div className="movie">
-          {movies.map((movie) => {
-            return (
-              <Movie
-                key={movie.id}
-                title={movie.title}
-                download_count={movie.download_count}
-                like_count={movie.like_count}
-                rating={movie.rating}
-                poster={movie.small_cover_image}
-              />
-            );
-          })}
-        </div>
-      </section>
+  render() {
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading..." : this.renderMovie()};</div>;
+  }
+
+  renderMovie() {
+    return(
+    return this.state.movies.map((movie)=> {
+      return (
+        <Movie
+          key={movie.id}
+          title={movie.title}
+          download_count={movie.download_count}
+          like_count={movie.like_count}
+          rating={movie.rating}
+          poster={movie.small_cover_image}
+          intro={movie.description_full}
+        />
+      );
+    })
     )
   }
 }
--- src/Movie.js
+++ src/Movie.js
@@ -1,16 +1,22 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function Movie({title, download_count, like_count, rating, poster}){
-    return <p>{title}</p>;
+function Movie({title, rating, poster}){
+    return(
+        <div className="movie_info">
+            <img src={poster} alt={title} tile={title} />
+            <h3>{title}</h3>
+            <h4>{rating}</h4>
+        </div>
+    )
 }
 
 Movie.propTypes = {
     title: PropTypes.string.isRequired,
-    download_count: PropTypes.number.isRequired,
-    like_count: PropTypes.number.isRequired,
     rating: PropTypes.number.isRequired,
-    poster: PropTypes.string.isRequired,
+    poster: PropTypes.string.isRequired
 }
+
+console.Movie();
 
 export default Movie;
